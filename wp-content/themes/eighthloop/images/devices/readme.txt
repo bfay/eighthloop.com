aec60613f0c04ef56b3be8b6b09614ba904d3173
Byron Fay <bfay_git@me.com>
2012-08-17 22:54:20 -0400

    started Fibonacci theme

--- wp-content/themes/eighthloop/images/devices/readme.txt
+++
@@ -1,4 +0,0 @@
-Images are used for iOS devices.
-
-Visit eighthloop Support Forum:
-http://themefortress.com/discuss/forum/eighthlooptheme/
--- wp-content/themes/eighthloop/sidebar.php
+++
@@ -1,5 +0,0 @@
-<aside id="sidebar" class="four columns sideborders" role="complementary">
-	<div class="right_sidebar">
-		<?php dynamic_sidebar("Sidebar"); ?>
-	</div>
-</aside><!-- /#sidebar -->
