f8fece8b35a05b070b9a8cb06bc5750432ea4164
Byron Fay <bfay_git@me.com>
2012-08-19 21:56:09 -0400

    putting together less framework with Gumby

--- wp-content/themes/fibonacci/header.php
+++ wp-content/themes/fibonacci/header.php
@@ -57,6 +57,9 @@
 	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/images/ipad_portrait_loading.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)" />
 	<!-- Startup Image iPhone (320x460) -->
 	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/images/iphone_load.png" media="screen and (max-device-width: 320px)" />
+	
+	<script type="text/javascript" src="//use.typekit.net/dpy2wej.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
  	
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />	
 	
