0821f5d224c17d822233d0eed4195f8374a909a0
Byron Fay <bfay_git@me.com>
2012-08-20 00:34:28 -0400

    Beginnings of whipping bootstrap into shape.  Started at the minimum css necessary so far.

--- wp-content/themes/fibonacci/header.php
+++ wp-content/themes/fibonacci/header.php
@@ -1,46 +1,78 @@
-<?php
-/**
- * The Header for our theme.
- *
- * Displays all of the <head> section and everything up till <div id="main">
- *
- * @package Fibonacci
- * @since Fibonacci 1.0
- */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
+<meta charset="UTF-8">
 
-	wp_title( '|', true, 'right' );
+	<title>Eighth Loop Creative Group</title>
+	<meta name="description" content="Creating responsive web sites other designers only dream of creating">
+ 
+	
+		<!-- Google Chrome Frame for IE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		
+		<!-- mobile meta (hooray!) -->
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<meta name="google-site-verification" content="Eighth Loop Creative Group">
+	<!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
+	
+	<meta name="keywords" content="foundation rapid prototyping, zurb foundation 3, design, frameworks, wordpress framework, css framework, html framework, html5, css3 transitions, css3, SASS, SCSS, compass, responsive design, hybrid responsive web design, RWD, HRWD, modernizr, categorizr, selectivizr, how to build a website, CodeKit, Coda 2, web developer, barberton ohio, north canton ohio, akron ohio, cleveland ohio, northeast ohio, web design barberton ohio, web design norton ohio, web design canton ohio, ">
+	<meta name="author" content="Byron Fay">
 
-	// Add the blog name.
-	bloginfo( 'name' );
+	<!-- Dublin Core Metadata : http://dublincore.org/ -->
+	<meta name="DC.title" content="Learning to Build a Hybrid Responsive Website Design (HRWD)">
+	<meta name="DC.subject" content="A web designer & developer full of youthful exuberance and curiousity which cannot be satisfied">
+	<meta name="DC.creator" content="Byron Fay">  
+				
+	<!-- Favicon and Feed -->
+	<link rel="shortcut icon" type="image/png" href="http://8images.eighthloop.com/favicon.png">
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+	
+	<!--  iPhone Web App Home Screen Icon -->
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/images/apple_icon_72.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/apple_icon_114.png" />
+	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/apple_icon_58.png" />
+	
+	<!-- Enable Startup Image for iOS Home Screen Web App -->
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/apple_icon_58.png" />
 
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
 
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'fibonacci' ), max( $paged, $page ) );
+	<!-- Startup Image iPad Landscape (748x1024) -->
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/images/ipad_landscape_loading.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)" />
+	<!-- Startup Image iPad Portrait (768x1004) -->
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/images/ipad_portrait_loading.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)" />
+	<!-- Startup Image iPhone (320x460) -->
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/images/iphone_load.png" media="screen and (max-device-width: 320px)" />
+	 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	
+				<!-- drop Google Analytics Here -->
+		<script type="text/javascript">
 
-	?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33695808-1']);
+  _gaq.push(['_setDomainName', 'eighthloop.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+		<!-- end analytics -->
 
 <?php wp_head(); ?>
+
 </head>
+
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
