7ff405bba3eb617d186dc945624c871b0decdc66
Byron Fay <bfay_git@me.com>
2012-08-20 22:46:38 -0400

    corrections to header and less compiling

---
+++ wp-content/themes/fibonacci/custom-front-page.php
@@ -0,0 +1,18 @@
+<?php
+/* 
+Template Name: Front Page
+ */
+
+get_header(); ?>
+
+		<div id="main-container" class="wrap row">
+			<div id="content" class="eleven columns centered" role="main">
+
+			
+			
+			
+			</div><!-- #content -->
+		</div><!-- #primary .site-content -->
+
+
+<?php get_footer(); ?>
